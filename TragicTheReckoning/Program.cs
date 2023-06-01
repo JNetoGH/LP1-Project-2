69886c71779e8d42d0bf91bd60c8b26353b65a5e
JNeto <joaomendes15@gmail.com>
2023-06-01 22:11:33 +0100

    Solution and project added

---
+++ TragicTheReckoning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TragicTheReckoning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
