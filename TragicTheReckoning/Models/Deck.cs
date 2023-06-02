6aa92f9773fcac68c4e6e6e55b2cb530269d9918
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-02 14:03:47 +0100

    Reworked Deck to implement a shuffle method which randomizes the cards' order

--- TragicTheReckoning/Models/Deck.cs
+++ TragicTheReckoning/Models/Deck.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace TragicTheReckoning
 {
     public class Deck
     {
-        public List<Card> Pool { get; private set; }
+        private List<Card> cardPool = new List<Card>()
+        {
+            new Card("Bugs", 2,3,4),
+            new Card("ijdeijdei", 2, 5, 7),
+            new Card("iejiejd", 4, 5, 8),
+            new Card("dkoekoe", 2, 3, 4),
+            new Card("cnnbnmbs", 3, 1, 6),
+            new Card("qwqryr", 7, 3, 1),
+        };
+
+        public List<Card> shuffledDeck { get; private set; } = new List<Card>();
+
         public List<Card> Hand { get; private set; }
 
         public void Shuffle()
         {
-            Pool.Sort();
+            Random rnd = new Random();
+
+            for(int i = cardPool.Count - 1; i>=0; i--)
+            {
+                int randomInt = rnd.Next(0,i);
+                shuffledDeck.Add(cardPool[randomInt]);
+                cardPool.RemoveAt(randomInt);
+            }
         }
     }
 }
