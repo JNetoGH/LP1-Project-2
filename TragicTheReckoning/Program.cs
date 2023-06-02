7e46b7a3730a5507d8eb3c7491128732731c9cbf
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-02 14:04:54 +0100

    Edit Program.cs to test the Shuffle method in Deck

--- TragicTheReckoning/Program.cs
+++ TragicTheReckoning/Program.cs
@@ -12,9 +12,34 @@ namespace TragicTheReckoning
             
             BattleView battleView = new BattleView(player1, player2);
             
-            battleView.RenderPlayerHand(player1);
-            battleView.RenderPlayerHand(player2);
+            //battleView.RenderPlayerHand(player1);
+            //battleView.RenderPlayerHand(player2);
 
+            Deck deck1 = new Deck();
+            Deck deck2 = new Deck();
+            Deck deck3 = new Deck();
+
+            deck1.Shuffle();
+            deck2.Shuffle();
+            deck3.Shuffle();
+            
+            foreach(Card i in deck1.shuffledDeck)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            foreach (Card i in deck2.shuffledDeck)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            foreach (Card i in deck3.shuffledDeck)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
         }
     }
 }
