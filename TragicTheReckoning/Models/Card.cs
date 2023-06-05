5a5e12167c5604837380b9e2a90cc3e57a723aae
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-05 14:23:15 +0100

    Add new variables to Card required for the Battle Phase

--- TragicTheReckoning/Models/Card.cs
+++ TragicTheReckoning/Models/Card.cs
@@ -4,20 +4,22 @@
     {   
         public string Name { get; private set; }
         public int Cost { get; private set; }
-        public int AttackPoints { get; private set; }
-        public int DefencePoints { get; private set; }
+        public int maxAttackPoints { get; private set; }
+        public int currentAttackPoints { get; set; }
+        public int maxDefencePoints { get; private set; }
+        public int currentDefencePoints { get; set; }
 
         public Card(string name, int cost, int attackPoints, int defencePoints)
         {
             Name = name;
             Cost = cost;
-            AttackPoints = attackPoints;
-            DefencePoints = defencePoints;
+            maxAttackPoints = attackPoints;
+            maxDefencePoints = defencePoints;
         }
 
         public override string ToString()
         {
-            return $"{Name} (Cost:{Cost}) (AP:{AttackPoints}) (DP:{DefencePoints})";
+            return $"{Name} (Cost:{Cost}) (AP:{maxAttackPoints}) (DP:{maxDefencePoints})";
         }
         
     }
