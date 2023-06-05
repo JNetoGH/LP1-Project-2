52753c4b4cda58b707c11eb65db5fdfba2bd09f7
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-05 14:23:42 +0100

    Add basic implementation of BattlePhase logic and its loop

--- TragicTheReckoning/Controllers/Phases/BattlePhase.cs
+++ TragicTheReckoning/Controllers/Phases/BattlePhase.cs
@@ -21,10 +21,104 @@ namespace TragicTheReckoning.Controllers.Phases
         public void RunPhase(int roundNumber, params Player[] players)
         {
             _battleView.RenderPhaseLabel(roundNumber, this.GetType());
-            Console.WriteLine("Running Battle phase (not done yet)");
+            BattlePhaseLoop(players);
             _battleView.RenderBattleView(players);
             
             _battleView.RenderPhaseExit(this.GetType());
+        }
+
+        public void BattlePhaseLoop(Player[] players)
+        {
+            foreach (Player player in players)
+            {
+                foreach (Card card in player.CardsInArena)
+                {
+                    card.currentAttackPoints = card.maxAttackPoints;
+                    card.currentDefencePoints = card.maxDefencePoints;
+                }   
+            }
+
+            while (_player1.CardsInArena.Count >= 0 | _player2.CardsInArena.Count >= 0)
+            {
+                Card player1Card = _player1.CardsInArena[0];
+                Card player2Card = _player2.CardsInArena[0];
+
+                if (player1Card.currentDefencePoints > 0 & player2Card.currentDefencePoints > 0)
+                {
+                    if (player1Card.currentAttackPoints >= player2Card.currentDefencePoints)
+                    {
+                        int currentAttackPoints = player1Card.currentAttackPoints;
+                        player1Card.currentAttackPoints -= player2Card.currentDefencePoints;
+                        player2Card.currentDefencePoints -= currentAttackPoints;
+                    }
+                    if (player2Card.currentAttackPoints >= player1Card.currentDefencePoints)
+                    {
+                        int currentAttackPoints = player2Card.currentAttackPoints;
+                        player2Card.currentAttackPoints -= player1Card.currentDefencePoints;
+                        player1Card.currentDefencePoints -= currentAttackPoints;
+                    }   
+                }
+
+                while(player1Card.currentAttackPoints > 0 & player1Card.currentDefencePoints > 0)
+                {
+                    int currentAttackPoints = player1Card.currentAttackPoints;
+                    int index = 1;
+                    
+                    player1Card.currentAttackPoints -= _player2.CardsInArena[index].currentDefencePoints;
+                    _player2.CardsInArena[index].currentDefencePoints -= currentAttackPoints;
+
+                    index++;
+                }
+                
+                while(player2Card.currentAttackPoints > 0 & player2Card.currentDefencePoints > 0)
+                {
+                    int currentAttackPoints = player2Card.currentAttackPoints;
+                    int index = 1;
+                    
+                    player2Card.currentAttackPoints -= _player1.CardsInArena[index].currentDefencePoints;
+                    _player1.CardsInArena[index].currentDefencePoints -= currentAttackPoints;
+
+                    index++;
+                }
+
+                foreach (Player player in players)
+                {
+                    foreach (Card card in player.CardsInArena)
+                    {
+                        if (card.currentDefencePoints <= 0)
+                        {
+                            player.CardsInArena.Remove(card);
+                        }
+                    }   
+                }
+
+                if (_player1.CardsInArena[0].currentAttackPoints <= 0 &
+                    _player2.CardsInArena[0].currentAttackPoints <= 0)
+                {
+                    break;
+                }
+            }
+
+            if(_player1.CardsInArena.Count >= 0 ^ _player2.CardsInArena.Count >= 0)
+            {
+                if (_player1.CardsInArena.Count >= 0)
+                {
+                    foreach (Card card in _player1.CardsInArena)
+                    {
+                        _player2.HealthPoints -= card.currentAttackPoints;
+                        _player1.CardsInArena.Remove(card);
+                    }
+                }
+                else
+                {
+                    foreach (Card card in _player2.CardsInArena)
+                    {
+                        _player1.HealthPoints -= card.currentAttackPoints;
+                        _player2.CardsInArena.Remove(card);
+                    }
+                }
+            }
+            
         }
     }
 }
