d871f476d89fd88236e53e3632dcb5bd57ec74b5
JNeto <joaomendes15@gmail.com>
2023-06-06 22:00:45 +0100

    player names are now set at the gameloop, there are no more view behaviour out of a view

--- TragicTheReckoning/Program.cs
+++ TragicTheReckoning/Program.cs
@@ -7,24 +7,11 @@ namespace TragicTheReckoning
 {
     class Program
     {
-        private static string _player1Name;
-        private static string _player2Name;
         
         static void Main()
         {
-            Console.Write("Please provide the name for Player1: ");
-            _player1Name = Console.ReadLine();
-            Console.Write("Please provide the name for Player2: ");
-            _player2Name = Console.ReadLine();
-            
-            GameLoop gameLoop = new GameLoop
-            (
-                new Player(_player1Name),
-                new Player(_player2Name)    
-            );
-            
+            GameLoop gameLoop = new GameLoop();
             gameLoop.Run();
-            
         }
     }
 }
