7f996a87e9e3ecda8da939c7722ec626c5d1c9f7
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-06 13:25:47 +0100

    Cleaned the code by organizing the loop with methods

--- TragicTheReckoning/Controllers/Phases/BattlePhase.cs
+++ TragicTheReckoning/Controllers/Phases/BattlePhase.cs
@@ -5,132 +5,137 @@ using TragicTheReckoning.Views;
 
 namespace TragicTheReckoning.Controllers.Phases
 {
-    public class BattlePhase: IPhase
+    public class BattlePhase : IPhase
     {
         private readonly BattleView _battleView;
         private Player _player1;
         private Player _player2;
-        
+
         public BattlePhase(Player player1, Player player2)
         {
             _battleView = new BattleView();
             _player1 = player1;
             _player2 = player2;
         }
-        
+
         public void RunPhase(int roundNumber, params Player[] players)
         {
             _battleView.RenderPhaseLabel(roundNumber, this.GetType());
             BattlePhaseLoop(players);
             _battleView.RenderBattleView(players);
-            
+
             _battleView.RenderPhaseExit(this.GetType());
         }
 
         public void BattlePhaseLoop(Player[] players)
+        {
+            ResetAttackValues(players);
+
+            while (_player1.CardsInArena.Count > 0 && _player2.CardsInArena.Count > 0)
+            {
+                Card player1Card = _player1.CardsInArena[0];
+                Card player2Card = _player2.CardsInArena[0];
+
+                if (player1Card.currentAttackPoints <= 0 && player2Card.currentAttackPoints <= 0)
+                    break;
+
+                if (player1Card.DefencePoints > 0 && player2Card.DefencePoints > 0)
+                {
+                    CardCombat(player1Card, player2Card);
+                }
+
+                ExtraDamage(player1Card, _player2);
+                ExtraDamage(player2Card, _player1);
+            }
+            
+            foreach (Player player in players)
+            {
+                RemoveDeadCards(players);
+            }
+
+            OneSidedResolver(_player1, _player2);
+
+            CheckDeadPlayer(_player1, _player2);
+        }
+
+        public void ResetAttackValues(Player[] players)
         {
             foreach (Player player in players)
             {
                 foreach (Card card in player.CardsInArena)
                 {
                     card.currentAttackPoints = card.maxAttackPoints;
-                }   
+                }
             }
-            
-            while (_player1.CardsInArena.Count > 0 && _player2.CardsInArena.Count > 0)
+        }
+
+        public void CardCombat(Card card1, Card card2)
+        {
+            int attackPoints = card1.currentAttackPoints;
+            card1.currentAttackPoints -= card2.DefencePoints;
+            card2.DefencePoints -= attackPoints;
+
+            attackPoints = card2.currentAttackPoints;
+            card2.currentAttackPoints -= card1.DefencePoints;
+            card1.DefencePoints -= attackPoints;
+        }
+
+        public void ExtraDamage(Card card, Player otherPlayer)
+        {
+            while (card.currentAttackPoints > 0 && card.DefencePoints > 0)
             {
-                Card player1Card = _player1.CardsInArena[0];
-                Card player2Card = _player2.CardsInArena[0];
-                
-                if (_player1.CardsInArena[0].currentAttackPoints <= 0 && 
-                    _player2.CardsInArena[0].currentAttackPoints <= 0)
+                int currentAttackPoints = card.currentAttackPoints;
+                int index = 1;
+
+                try
+                {
+                    card.currentAttackPoints -= otherPlayer.CardsInArena[index].DefencePoints;
+                    otherPlayer.CardsInArena[index].DefencePoints -= currentAttackPoints;
+                }
+                catch
                 {
                     break;
                 }
-
-                if (player1Card.DefencePoints > 0 && player2Card.DefencePoints > 0)
-                {
-                    int p1AttackPoints = player1Card.currentAttackPoints;
-                    player1Card.currentAttackPoints -= player2Card.DefencePoints;
-                    player2Card.DefencePoints -= p1AttackPoints;
-                    
-                    int p2AttackPoints = player2Card.currentAttackPoints;
-                    player2Card.currentAttackPoints -= player1Card.DefencePoints;
-                    player1Card.DefencePoints -= p2AttackPoints;
-                }
-
-                while(player1Card.currentAttackPoints > 0 && player1Card.DefencePoints > 0)
-                {
-                    int currentAttackPoints = player1Card.currentAttackPoints;
-                    int index = 1;
-
-                    try
-                    {
-                        player1Card.currentAttackPoints -= _player2.CardsInArena[index].DefencePoints;
-                        _player2.CardsInArena[index].DefencePoints -= currentAttackPoints;
-                    }
-                    catch
-                    {
-                        break;
-                    }
-
-                    index++;
-                }
                 
-                while(player2Card.currentAttackPoints > 0 && player2Card.DefencePoints > 0)
-                {
-                    int currentAttackPoints = player2Card.currentAttackPoints;
-                    int index = 1;
-
-                    try
-                    {
-                        player2Card.currentAttackPoints -= _player1.CardsInArena[index].DefencePoints;
-                        _player1.CardsInArena[index].DefencePoints -= currentAttackPoints;
-                    }
-                    catch
-                    {
-                        break;
-                    }
-
-                    index++;
-                }
-
-                foreach (Player player in players)
-                {
-                    if (player.HealthPoints <= 0)
-                    {
-                        throw new Exception();
-                    }
-
-                    player.CardsInArena.RemoveAll(card => card.DefencePoints <= 0);
-                }
+                index++;
             }
+        }
 
-            if(_player1.CardsInArena.Count > 0 ^ _player2.CardsInArena.Count > 0)
+        public void RemoveDeadCards(Player[] players)
+        {
+            foreach (Player player in players)
             {
-                if (_player1.CardsInArena.Count > 0)
-                {
-                    foreach (Card card in _player1.CardsInArena)
-                    {
-                        _player2.HealthPoints -= card.currentAttackPoints;
-                    }
-                    _player1.CardsInArena.Clear();
-                }
-                else
-                {
-                    foreach(Card card in _player2.CardsInArena)
-                    {
-                        _player1.HealthPoints -= card.currentAttackPoints;
-                    }
-                    _player2.CardsInArena.Clear();
-                }
+                player.CardsInArena.RemoveAll(card => card.DefencePoints <= 0);
             }
+        }
 
+        public void OneSidedResolver(Player player1, Player player2)
+        {
+            if (!(player1.CardsInArena.Count > 0 ^ player2.CardsInArena.Count > 0)) return;
+            if (player1.CardsInArena.Count > 0)
+            {
+                foreach (Card card in player1.CardsInArena)
+                {
+                    _player2.HealthPoints -= card.currentAttackPoints;
+                }
+                _player1.CardsInArena.Clear();
+            }
+            else
+            {
+                foreach (Card card in _player2.CardsInArena)
+                {
+                    _player1.HealthPoints -= card.currentAttackPoints;
+                }
+                _player2.CardsInArena.Clear();
+            }
+        }
+
+        public void CheckDeadPlayer(Player player1, Player player2)
+        {
             if (_player1.HealthPoints <= 0 || _player2.HealthPoints <= 0)
             {
                 throw new Exception();
             }
-            
         }
     }
 }
