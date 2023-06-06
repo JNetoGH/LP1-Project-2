443937dfc354e37626610296f29f537879b73c88
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-06-06 19:25:46 +0100

    Add comments to BattleView and BattlePhase

--- TragicTheReckoning/Controllers/Phases/BattlePhase.cs
+++ TragicTheReckoning/Controllers/Phases/BattlePhase.cs
@@ -5,19 +5,34 @@ using TragicTheReckoning.Views;
 
 namespace TragicTheReckoning.Controllers.Phases
 {
+    /// <summary>
+    /// Handles the Battle Phase and calls the BattleView to present the information to the player
+    /// </summary>
     public class BattlePhase : IPhase
     {
+        //Declaring the variables
         private readonly BattleView _battleView;
         private Player _player1;
         private Player _player2;
 
+        /// <summary>
+        /// Initializes the variables
+        /// </summary>
+        /// <param name="player1">Reference to Player 1</param>
+        /// <param name="player2">Reference to Player 2</param>
         public BattlePhase(Player player1, Player player2)
         {
+            //Initializes the variables
             _battleView = new BattleView();
             _player1 = player1;
             _player2 = player2;
         }
 
+        /// <summary>
+        /// Calls the components of the Battle Phase in order
+        /// </summary>
+        /// <param name="roundNumber">The number of the current round</param>
+        /// <param name="players">Receives an array containing the players</param>
         public void RunPhase(int roundNumber, params Player[] players)
         {
             _battleView.RenderPhaseLabel(roundNumber, this.GetType());
@@ -25,139 +40,232 @@ namespace TragicTheReckoning.Controllers.Phases
             _battleView.RenderPhaseExit(this.GetType());
         }
 
+        /// <summary>
+        /// Handles the logic portion of the Battle Phase and calls the render for that logic
+        /// </summary>
+        /// <param name="players">Receives an array containing the players</param>
         private void BattlePhaseLoop(Player[] players)
         {
+            //Resets a card's AttackPoints to the maxAttackPoints in case the cards have survived from a previous round
             ResetAttackValues(players);
             
+            //Showcases the cards in the field to the players
             _battleView.RenderCardsInField(players);
 
+            //While both players have cards in the arena
             while (_player1.CardsInArena.Count > 0 && _player2.CardsInArena.Count > 0)
             {
+                //Initialize variables containing the first cards in play
                 Card player1Card = _player1.CardsInArena[0];
                 Card player2Card = _player2.CardsInArena[0];
 
+                //If both players' first cards have no remaining Attack Points
                 if (player1Card.currentAttackPoints <= 0 && player2Card.currentAttackPoints <= 0)
                 {
+                    //Write that the cards cannot kill each other
                     _battleView.NoDamageDealt(_player1, player1Card, _player2, player2Card);
+                    //Break out from the Battle Phase loop
                     break;
                 }
 
+                //If both players' first cards still have Defence Points
                 if (player1Card.DefencePoints > 0 && player2Card.DefencePoints > 0)
                 {
+                    //Handle the combat between both cards
                     CardCombat(player1Card, player2Card);
                 }
 
+                //If a card still has remaining Attack Points and Defence Points, it deals
+                //damage to the following cards until it exhausts its Attack Points 
                 ExtraDamage(_player1, player1Card, _player2);
                 ExtraDamage(_player2, player2Card, _player1);
                 
+                //Iterate through each player
                 foreach (Player player in players)
                 {
+                    //Remove all cards, that no longer have Defence Points, from play
                     RemoveDeadCards(players);
                 }
             }
 
+            //If only one player has cards in play, they deal damage to the other player directly and are then removed
             OneSidedResolver(_player1, _player2);
 
+            //Checks if either player has died and if yes, it ends the GameLoop
             CheckDeadPlayer(_player1, _player2);
         }
 
+        /// <summary>
+        /// Iterates through the cards in play of each player and resets their AttackPoints to the maxAttackPoints
+        /// in case the cards have survived from a previous round
+        /// </summary>
+        /// <param name="players">Receives an array containing the players</param>
         private void ResetAttackValues(Player[] players)
         {
+            //Iterate through each player
             foreach (Player player in players)
             {
+                //Iterate through each card in play
                 foreach (Card card in player.CardsInArena)
                 {
+                    //Reset their current AttackPoints to their default value
                     card.currentAttackPoints = card.maxAttackPoints;
                 }
             }
         }
 
+        /// <summary>
+        /// Handles the combat logic between two cards
+        /// </summary>
+        /// <param name="card1">Reference to a card</param>
+        /// <param name="card2">Reference to the opposing card</param>
         private void CardCombat(Card card1, Card card2)
         {
+            //Store the Attack Points of the card
             int attackPoints = card1.currentAttackPoints;
+            //Inform the players of what's happening and the values of it
             _battleView.RenderDamageDealt(_player1, card1, _player2, card2);
+            //Remove the opposing card's Defence Points to the current card's Attack Points
             card1.currentAttackPoints -= card2.DefencePoints;
+            //Remove the stored Attack Points from the opposing card's Attack Points
             card2.DefencePoints -= attackPoints;
 
+            //Repeats the operation above on the other way around
             attackPoints = card2.currentAttackPoints;
             _battleView.RenderDamageDealt(_player2, card2, _player1, card1);
             card2.currentAttackPoints -= card1.DefencePoints;
             card1.DefencePoints -= attackPoints;
         }
 
+        /// <summary>
+        /// If a card still has remaining Attack Points and Defence Points, it continually deals damage to
+        /// the following cards until it exhausts its Attack Points
+        /// </summary>
+        /// <param name="player">Reference to the Player who controls the card</param>
+        /// <param name="card">Reference to the Card in play</param>
+        /// <param name="otherPlayer">Reference to the opposing Player</param>
         private void ExtraDamage(Player player, Card card, Player otherPlayer)
         {
+            //Initialize an index variable at 1
             int index = 1;
             
-            while (card.currentAttackPoints > 0 && card.DefencePoints > 0)
+            //While the card still has remaining AttackPoints and Defence Points
+            while(card.currentAttackPoints > 0 && card.DefencePoints > 0)
             {
+                //Store the card's currentAttackPoints
                 int currentAttackPoints = card.currentAttackPoints;
 
+                //Try to perform the following operations
                 try
                 {
+                    //Inform the players of the logic and the values of what happens in this step
                     _battleView.RenderDamageDealt(player, card, otherPlayer,
                         otherPlayer.CardsInArena[index]);
                     
+                    //Remove the Defense Points of the other card from the Attack Points of the card
                     card.currentAttackPoints -= otherPlayer.CardsInArena[index].DefencePoints;
+                    //Remove the stored attack points from the other card's Defense Points
                     otherPlayer.CardsInArena[index].DefencePoints -= currentAttackPoints;
                 }
+                //If the index ever falls out of range
                 catch
                 {
+                    //Exit out of this loop
                     break;
                 }
                 
+                //Increment the index variables
                 index++;
             }
         }
 
+        /// <summary>
+        /// Removes all cards with 0 or less Defense Points from the game
+        /// </summary>
+        /// <param name="players">Receives an array containing the players</param>
         private void RemoveDeadCards(Player[] players)
         {
+            //Iterate through both players
             foreach (Player player in players)
             {
+                //Remove all cards that have 0 or less Defense Points from the game
                 player.CardsInArena.RemoveAll(card => card.DefencePoints <= 0);
             }
         }
 
+        /// <summary>
+        /// If only one players has cards remaining, they deal their damage directly to the opposing
+        /// player and then get removed from the game
+        /// </summary>
+        /// <param name="player1">Reference to Player 1</param>
+        /// <param name="player2">Reference to Player 2</param>
         private void OneSidedResolver(Player player1, Player player2)
         {
+            //If there's not only one Player with cards in play, exit out of this method
             if (!(player1.CardsInArena.Count > 0 ^ player2.CardsInArena.Count > 0)) return;
             
+            //If Player 1 has cards in play
             if (player1.CardsInArena.Count > 0)
             {
+                //Inform the players that only Player 1 has cards in play
                 _battleView.OnlyOnePlayerHasCards(player1);
                 
+                //For every card in play
                 foreach (Card card in player1.CardsInArena)
                 {
+                    //Inform the player's that the card will deal X damage directly to the other player
                     _battleView.DirectDamageDealt(player1, card, player2);
+                    //Have that card deal damage directly to the other player's Health Points
                     _player2.HealthPoints -= card.currentAttackPoints;
                 }
+                //Inform the players that every card will be removed from play
                 _battleView.ExhaustedCards(player1);
+                //Remove all cards from play
                 _player1.CardsInArena.Clear();
             }
+            //If Player 2 has cards in play
             else
             {
+                //Inform the players that only Player 1 has cards in play
                 _battleView.OnlyOnePlayerHasCards(player2);
                 
+                //For every card in play
                 foreach (Card card in _player2.CardsInArena)
                 {
+                    //Inform the player's that the card will deal X damage directly to the other player
                     _battleView.DirectDamageDealt(player2, card, player1);
+                    //Have that card deal damage directly to the other player's Health Points
                     _player1.HealthPoints -= card.currentAttackPoints;
                 }
+                //Inform the players that every card will be removed from play
                 _battleView.ExhaustedCards(player2);
+                //Remove all cards from play
                 _player2.CardsInArena.Clear();
             }
         }
 
+        /// <summary>
+        /// Checks if either players has died and terminates the GameLoop
+        /// </summary>
+        /// <param name="player1">Reference to Player 1</param>
+        /// <param name="player2">Reference to Player 1</param>
+        /// <exception cref="Exception"></exception>
         private void CheckDeadPlayer(Player player1, Player player2)
         {
+            //If Player 1's Health Points are 0 or lower
             if (_player1.HealthPoints <= 0)
             {
+                //Inform the players of who died
                 _battleView.RenderPlayerDeath(_player1);
+                //Throw the exception which will terminate the GameLoop
                 throw new Exception();
             }
+            //If Player 2's Health Points are 0 or lower
             else if (_player2.HealthPoints <= 0)
             {
+                //Inform the players of who died
                 _battleView.RenderPlayerDeath(_player2);
+                //Throw the exception which will terminate the GameLoop
                 throw new Exception();
             }
         }
--- TragicTheReckoning/Views/BattleView.cs
+++ TragicTheReckoning/Views/BattleView.cs
@@ -5,58 +5,107 @@ namespace TragicTheReckoning.Views
 {
     public class BattleView: View
     {
+        /// <summary>
+        /// Writes which Player has died
+        /// </summary>
+        /// <param name="player">Reference a Player</param>
         public void RenderPlayerDeath(Player player)
         {
+            //Writes which Player has died
             Console.WriteLine(player.Name + " has died!");
         }
 
+        /// <summary>
+        /// Writes all the cards in play from both players 
+        /// </summary>
+        /// <param name="players">Receives an array containing references of both Players</param>
         public void RenderCardsInField(Player[] players)
         {
+            //Iterate through each player
             foreach (Player player in players)
             {
+                //Write which Player this cards belong to
                 Console.WriteLine(player.Name + "'s Cards:");
                 int cardNumber = 0;
                 
+                //Iterate through each card
                 foreach (Card card in player.CardsInArena)
                 {
+                    //Write the name of each Card
                     cardNumber++;
                     Console.WriteLine("     " + cardNumber + ": " + card);
                 }
-                
+                //Write an empty line for spacing purposes 
                 Console.WriteLine();
             }
         }
 
+        /// <summary>
+        /// Write that neither card is able to deal damage to each other
+        /// </summary>
+        /// <param name="player1">A Player</param>
+        /// <param name="card1">The first Player's card</param>
+        /// <param name="player2">The other Player</param>
+        /// <param name="card2">The other Player's card</param>
         public void NoDamageDealt(Player player1, Card card1, Player player2, Card card2)
         {
+            //Write that neither card is able to deal damage to each other
             Console.WriteLine(player1.Name + "'s " + card1.Name + " battles " + player2.Name 
                               + "'s " + card2.Name + " and neither manages to kill the other.");
         }
 
+        /// <summary>
+        /// Write what card dealt damage to which one and how much it dealt
+        /// </summary>
+        /// <param name="player1">A Player</param>
+        /// <param name="card1">The first Player's card</param>
+        /// <param name="player2">The other Player</param>
+        /// <param name="card2">The other Player's card</param>
         public void RenderDamageDealt(Player player1, Card card1, Player player2, Card card2)
         {
+            //Store the damage that the card dealt to the other so it can be written
             int damage = card1.currentAttackPoints >= card2.DefencePoints ? card2.DefencePoints 
                 : card1.currentAttackPoints;
             
+            //Write what card dealt damage to which one and how much it dealt
             Console.WriteLine(player1.Name + "'s " + card1.Name + " deals " +
                               damage + " damage to " + player2.Name + "'s " + card2.Name);
         }
 
+        /// <summary>
+        /// Writes which Player only has cards remaining
+        /// </summary>
+        /// <param name="player">A Player</param>
         public void OnlyOnePlayerHasCards(Player player)
         {
+            //Writes which Player only has cards remaining
             Console.WriteLine("As only " + player.Name + " has monsters remaining, they launch an all-out attack!");
+            //Wait for an Input from the Player before proceeding
             RenderExitWithInput();
+            //Clears the Screen
             ClearScreen();
         }
 
+        /// <summary>
+        /// Writes which card dealt damage directly to which Player and how much it did
+        /// </summary>
+        /// <param name="player">A Player</param>
+        /// <param name="card">The Player's Card</param>
+        /// <param name="otherPlayer">The other Player</param>
         public void DirectDamageDealt(Player player, Card card, Player otherPlayer)
         {
+            //Writes which card dealt damage directly to which Player and how much it did
             Console.WriteLine(player.Name + "'s " + card.Name + " attacks " + otherPlayer.Name +
                               " directly, dealing " + card.currentAttackPoints + " damage!");
         }
 
+        /// <summary>
+        /// Writes that a Player's cards have been removed from the battlefield
+        /// </summary>
+        /// <param name="player">A Player</param>
         public void ExhaustedCards(Player player)
         {
+            //Writes that a Player's cards have been removed from the battlefield
             Console.WriteLine("Following the all-out attack, " + player.Name +
                               "'s monsters are exhausted and retire from the battlefield.");
         }
